c1908d6467f2a33f2d97d2ba61640f0ebce2ea75
coolslugma <stefanosstefanidis55@yahoo.com>
2025-05-07 21:19:35 +0300

    Add almost-complete decomp.

--- ntdll/RtlGetOSProductName.c
+++ ntdll/RtlGetOSProductName.c
@@ -1,22 +1,99 @@
-/*
- * Flags to replicate different versions of the function:
- * EARLY_LH: Longhorn builds 3683-4020
- * PIGLATIN: Longhorn builds 4029-4042 (main.030905-1800)
- * LH_2004: Longhorn builds 4042 (Lab06_n.030909-1709) - 4093
- *
- * You can only define one out of the three available.
- */
-//#define EARLY_LH	1
-//#define PIGLATIN	1
-#define LH_2004		1
-#if !defined(EARLY_LH) || !defined(PIGLATIN) || !defined(LH_2004)
-#error "You need to specify a specific Longhorn variant."
-#endif
+#include <ntifs.h>
+
+#include "lh_branding.h"
+
+VOID NTAPI
+AppendConditionally(PUNICODE_STRING Destination, BOOLEAN Condition, LPCWSTR Suffix)
+{
+    NTSTATUS Status;
+
+    if (Condition) {
+        Status = RtlAppendUnicodeToString(Destination, L" ");
+        if (!NT_SUCCESS(Status))
+		    return;
+    }
+
+    RtlAppendUnicodeToString(Destination, Suffix);
+}
 
 /*
+ * @implemented
+ *
+ * ntoskrnl.exe
+ * 0x16ed38  1157  RtlGetOSProductName
+ *
  * ntdll.dll
  * 0x5b678  1182  RtlGetOSProductName
- *
- * CURRENTLY A STUB
  */
+NTSTATUS NTAPI
+RtlGetOSProductName(OUT PUNICODE_STRING Buffer, IN ULONG Flags)
+{
+    NTSTATUS Status;
+    LPWSTR ProductName, ProductType;
+    OSVERSIONINFOW VersionInfo;
 
+    if (!Flags)
+        return STATUS_INVALID_PARAMETER;
+
+    // BUG: This is absolutely horrendous..why would MS do this?
+    // They check the second byte of the buffer for a NUL char, instead
+    // of its size, and if it is, it reports that the buffer is too
+    // small...
+    if (Buffer->Buffer[1] == L'\0')
+        return STATUS_BUFFER_TOO_SMALL;
+
+    Buffer->Buffer[0] = L'\0';
+    Buffer->Length = 0;
+
+    VersionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
+    Status = RtlGetVersion(&VersionInfo);
+    if (Status < 0)
+        return Status;
+
+    // Company information.
+    if (Flags & OS_PRODUCTNAME_COMPANY)
+        AppendConditionally(Buffer, FALSE, L"Microsoft");
+
+    // Generic operating system family.
+    if (Flags & OS_PRODUCTNAME_FAMILY_GENERIC)
+        AppendConditionally(Buffer, TRUE, L"Windows");
+
+    // Specific operating system family.
+    else if (Flags & OS_PRODUCTNAME_FAMILY_SPECIFIC) {
+        ProductName = (LPWSTR)L"Longhorn";
+        AppendConditionally(Buffer, TRUE, ProductName);
+    }
+
+    // Product type (aka edition).
+    if (Flags & OS_PRODUCTNAME_TYPE) {
+        ProductType = NULL;
+        if (!(VersionInfo.dwMajorVersion & 0x40)) {
+			if (VersionInfo.dwMajorVersion & 0x200)
+			    ProductType = (LPWSTR)L"Home Edition";
+			else if (((CHAR *)&VersionInfo.dwMajorVersion)[2] == 1)
+			    ProductType = (LPWSTR)L"Professional";
+			else if (VersionInfo.dwMajorVersion & 0x400)
+			    ProductType = (LPWSTR)L"Web Server";
+			else if (VersionInfo.dwMajorVersion & 0x21)
+			    ProductType = (LPWSTR)L"Small Business Server";
+			else if ((CHAR)VersionInfo.dwMajorVersion < 0)
+			    ProductType = (LPWSTR)L"Datacenter Server";
+			else if (VersionInfo.dwMajorVersion & 0x02)
+			    ProductType = (LPWSTR)L"Enterprise Server";
+            else
+                ProductType = (LPWSTR)L"Standard Server";
+        }
+		else
+            ProductType = (LPWSTR)L"Embedded";
+
+        AppendConditionally(Buffer, TRUE, ProductType);
+    }
+
+    if (Flags & OS_PRODUCTNAME_VERSION)
+        AppendConditionally(Buffer, TRUE, L"Version 2003");
+
+    if (Flags & OS_PRODUCTNAME_COPYRIGHT)
+        AppendConditionally(Buffer, TRUE, L"Copyright © 1985-2003 Microsoft Corporation");
+
+    return STATUS_SUCCESS;
+}
--- ntoskrnl/RtlGetOSProductName.c
+++ ntoskrnl/RtlGetOSProductName.c
@@ -1,31 +1,99 @@
-/*
- * This file is a part of the lh_branding project.
- */
+#include <ntifs.h>
 
-static void
-append_str(PUNICODE_STRING dest, char str, PCWSTR src)
+#include "lh_branding.h"
+
+VOID NTAPI
+AppendConditionally(PUNICODE_STRING Destination, BOOLEAN Condition, LPCWSTR Suffix)
 {
-	if ((str != '\0') && (RtlAppendUnicodeToString(dest, L" ") < 0))
-		return;
+    NTSTATUS Status;
 
-	RtlAppendUnicodeToString(dest, src);
+    if (Condition) {
+        Status = RtlAppendUnicodeToString(Destination, L" ");
+        if (!NT_SUCCESS(Status))
+		    return;
+    }
+
+    RtlAppendUnicodeToString(Destination, Suffix);
 }
 
 /*
+ * @implemented
+ *
  * ntoskrnl.exe
  * 0x16ed38  1157  RtlGetOSProductName
  *
- * CURRENTLY A STUB
+ * ntdll.dll
+ * 0x5b678  1182  RtlGetOSProductName
  */
-int
-RtlGetOSProductName(PUNICODE_STRING buf, OS_PRODUCTNAME productName)
+NTSTATUS NTAPI
+RtlGetOSProductName(OUT PUNICODE_STRING Buffer, IN ULONG Flags)
 {
-	dest = buf;
-	if (productName == 0)
-		return 
-	if (buf->MaximumLength == 0)
-		return STATUS_INVALID_PARAMETER;
+    NTSTATUS Status;
+    LPWSTR ProductName, ProductType;
+    OSVERSIONINFOW VersionInfo;
 
-	*buf->Buffer = L'\0';
-	buf->Length = 0;
+    if (!Flags)
+        return STATUS_INVALID_PARAMETER;
+
+    // BUG: This is absolutely horrendous..why would MS do this?
+    // They check the second byte of the buffer for a NUL char, instead
+    // of its size, and if it is, it reports that the buffer is too
+    // small...
+    if (Buffer->Buffer[1] == L'\0')
+        return STATUS_BUFFER_TOO_SMALL;
+
+    Buffer->Buffer[0] = L'\0';
+    Buffer->Length = 0;
+
+    VersionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
+    Status = RtlGetVersion(&VersionInfo);
+    if (Status < 0)
+        return Status;
+
+    // Company information.
+    if (Flags & OS_PRODUCTNAME_COMPANY)
+        AppendConditionally(Buffer, FALSE, L"Microsoft");
+
+    // Generic operating system family.
+    if (Flags & OS_PRODUCTNAME_FAMILY_GENERIC)
+        AppendConditionally(Buffer, TRUE, L"Windows");
+
+    // Specific operating system family.
+    else if (Flags & OS_PRODUCTNAME_FAMILY_SPECIFIC) {
+        ProductName = (LPWSTR)L"Longhorn";
+        AppendConditionally(Buffer, TRUE, ProductName);
+    }
+
+    // Product type (aka edition).
+    if (Flags & OS_PRODUCTNAME_TYPE) {
+        ProductType = NULL;
+        if (!(VersionInfo.dwMajorVersion & 0x40)) {
+			if (VersionInfo.dwMajorVersion & 0x200)
+			    ProductType = (LPWSTR)L"Home Edition";
+			else if (((CHAR *)&VersionInfo.dwMajorVersion)[2] == 1)
+			    ProductType = (LPWSTR)L"Professional";
+			else if (VersionInfo.dwMajorVersion & 0x400)
+			    ProductType = (LPWSTR)L"Web Server";
+			else if (VersionInfo.dwMajorVersion & 0x21)
+			    ProductType = (LPWSTR)L"Small Business Server";
+			else if ((CHAR)VersionInfo.dwMajorVersion < 0)
+			    ProductType = (LPWSTR)L"Datacenter Server";
+			else if (VersionInfo.dwMajorVersion & 0x02)
+			    ProductType = (LPWSTR)L"Enterprise Server";
+            else
+                ProductType = (LPWSTR)L"Standard Server";
+        }
+		else
+            ProductType = (LPWSTR)L"Embedded";
+
+        AppendConditionally(Buffer, TRUE, ProductType);
+    }
+
+    if (Flags & OS_PRODUCTNAME_VERSION)
+        AppendConditionally(Buffer, TRUE, L"Version 2003");
+
+    if (Flags & OS_PRODUCTNAME_COPYRIGHT)
+        AppendConditionally(Buffer, TRUE, L"Copyright © 1985-2003 Microsoft Corporation");
+
+    return STATUS_SUCCESS;
 }
